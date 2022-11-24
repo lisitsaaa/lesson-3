ffed595a05be54f55424352811acfceeba8877c1
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-11-24 14:12:50 +0300

    task 1.1

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,69 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int answer = 1;
+        int num;
 
         while (answer != 15) {
-            System.out.println("CONTROL STRUCTURES\t\tCYCLES\t\t\t\tADDITIONAL TASKS" +
+            System.out.print("CONTROL STRUCTURES\t\tCYCLES\t\t\t\tADDITIONAL TASKS" +
                     "\n1 - task 1.1\t\t\t6 - task 2.1\t\t12 - task 3.1" +
                     "\n2 - task 1.2\t\t\t7 - task 2.2\t\t13 - task 3.2" +
                     "\n3 - task 1.3\t\t\t8 - task 2.3\t\t14 - task 3.3" +
                     "\n4 - task 1.4\t\t\t9 - task 2.4" +
                     "\n5 - task 1.5\t\t\t10 - task 2.5" +
                     "\n\t\t\t\t\t\t11 - task 2.6" +
-                    "\n------------------------------------------------>15 - EXIT");
+                    "\n------------------------------------------------>15 - EXIT" +
+                    "\nYour choice: ");
             answer = input.nextInt();
 
             switch (answer) {
                 case 1:
-                    System.out.println("Task 1.1");
+                    System.out.print("Task 1.1\n Enter a number from 1 to 12: ");
+                    num = input.nextInt();
+                    switch (num){
+                        case 1:
+                            System.out.println("It's January");
+                            break;
+                        case 2:
+                            System.out.println("It's February");
+                            break;
+                        case 3:
+                            System.out.println("It's March");
+                            break;
+                        case 4:
+                            System.out.println("It's April");
+                            break;
+                        case 5:
+                            System.out.println("It's May");
+                            break;
+                        case 6:
+                            System.out.println("It's June");
+                            break;
+                        case 7:
+                            System.out.println("It's July");
+                            break;
+                        case 8:
+                            System.out.println("It's August");
+                            break;
+                        case 9:
+                            System.out.println("It's September");
+                            break;
+                        case 10:
+                            System.out.println("It's October");
+                        case 11:
+                            System.out.println("It's November");
+                            break;
+                        case 12:
+                            System.out.println("It's December");
+                            break;
+                        default:
+                            System.out.println("You wrote down an incorrect number. Try again :)");
+                            break;
+                    }
                     break;
 
                 case 2:
-                    System.out.println("Task 1.2" +
-                            "\nEnter a number from 1 to 12: ");
-                    int num = input.nextInt();
+                    System.out.print("Task 1.2\nEnter a number from 1 to 12: ");
+                    num = input.nextInt();
                     if (num == 1) {
                         System.out.println("It's January");
                     } else if (num == 2) {
